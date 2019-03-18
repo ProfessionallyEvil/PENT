ff252443d60760030ddc5d95a97cc74db022e198
alex <10230166+elreydetoda@users.noreply.github.com>
2019-03-18 02:42:35 -0400

    removing so can add https not ssh submodules

--- linux/vuln_web/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-vagrant up
-sleep 3
-vagrant halt
-vagrant package --base $(vboxmanage list vms | grep $(cat .vagrant/machines/default/virtualbox/id) | tr -d '"|{|}' | cut -d ' ' -f 1) --output vuln_web.box
-
-# tmp for testing
-vagrant box add --name PENT/vuln_web ./vuln_web.box
